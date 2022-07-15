dd32c0064eee930dd9192ffc78f8deabe1a799f2
ethan <blazer030@gmail.com>
2022-07-15 22:11:52 +0800

    update 2022-07-15 22:11:52

---
+++ js/about.2a6344a8.js
@@ -0,0 +1,2 @@
+"use strict";(self["webpackChunksudoku"]=self["webpackChunksudoku"]||[]).push([[443],{7381:function(u,n,s){s.r(n),s.d(n,{default:function(){return f}});var t=s(3396);const a={class:"about"},e=(0,t._)("h1",null,"This is an about page",-1),r=[e];function c(u,n){return(0,t.wg)(),(0,t.iD)("div",a,r)}var o=s(89);const i={},d=(0,o.Z)(i,[["render",c]]);var f=d}}]);
+//# sourceMappingURL=about.2a6344a8.js.map
