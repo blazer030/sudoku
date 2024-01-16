aa67d4fd6731a96865f1442a0f1a44449a1a8be0
ethan <blazer030@gmail.com>
2024-01-17 00:44:29 +0800

    extract component

--- src/presentation/App.tsx
+++ src/presentation/App.tsx
@@ -1,10 +1,8 @@
 import { Outlet } from "react-router-dom";
-import { Button } from "@/presentation/components/ui/button.tsx";
 
 function App() {
     return (
         <div className="container mx-auto">
-            <Button>hello</Button>
             <Outlet />
         </div>
     );
--- src/routerConfig.tsx
+++ src/routerConfig.tsx
@@ -1,12 +1,23 @@
 import { createBrowserRouter, Navigate } from "react-router-dom";
 import App from "@/presentation/App.tsx";
+import { Button } from "@/presentation/components/ui/button.tsx";
+
+const Home = () => {
+    const buttonName: string = "hello";
+    return <div>
+        home
+        <Button variant="outline" className="rounded-full">
+            {buttonName}
+        </Button>
+    </div>;
+};
 
 const router = createBrowserRouter([{
     path: "/",
     element: <App />,
     children: [{
         path: "/",
-        element: <div>home</div>,
+        element: <Home />,
     }, {
         path: "/game",
         element: <div>game</div>,
