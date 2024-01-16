30898c8a821f1e0c7877634e780cf24c5be8f6e4
ethan <blazer030@gmail.com>
2024-01-17 00:52:06 +0800

    navigate to game

--- src/presentation/pages/home/Home.tsx
+++ src/presentation/pages/home/Home.tsx
@@ -1,11 +1,16 @@
 import { Button } from "@/presentation/components/ui/button.tsx";
+import { useNavigate } from "react-router-dom";
 
 export const Home = () => {
-    const buttonName: string = "hello";
+    const navigate = useNavigate();
+
     return <div>
         home
-        <Button variant="outline" className="rounded-full">
-            {buttonName}
+        <Button variant="outline"
+                className="rounded-full"
+                onClick={() => navigate("/game")}
+        >
+            Start
         </Button>
     </div>;
 };
