d6f7b1ec60ad83b362b18435fd1be95df68724af
ethan <blazer030@gmail.com>
2024-01-18 09:57:19 +0800

    move ui lib

--- src/presentation/components/ui/button/button.tsx
+++ src/presentation/components/ui/button/button.tsx
@@ -2,7 +2,7 @@ import * as React from "react";
 import { Slot } from "@radix-ui/react-slot";
 import { type VariantProps } from "class-variance-authority";
 
-import { cn } from "@/presentation/lib/utils";
+import { cn } from "@/presentation/components/ui/lib/utils";
 import buttonVariants from "@/presentation/components/ui/button/variants";
 
 export interface ButtonProps
