9eb687ca50084e2caf4a22edf40e4471a16ee1b4
ethan <blazer030@gmail.com>
2024-01-20 00:20:57 +0800

    static Home page

--- src/presentation/pages/home/Home.tsx
+++ src/presentation/pages/home/Home.tsx
@@ -7,32 +7,48 @@ export const Home = () => {
     const navigate = useNavigate();
 
     return <div className="flex flex-col h-full justify-center items-center gap-4 px-12">
-        <div className="mb-32">
+        <div className="mb-32 rounded-full bg-sky-200 p-4 text-white">
             <Icon type={ICON_TYPE.Logo} size="10rem" />
         </div>
 
-        <Button variant="outline"
-                className="rounded-full w-full"
-                onClick={() => {
-                    navigate("/game");
-                }}
+        <Button
+            variant="outline"
+            className="rounded-full w-full text-gray-500"
+            onClick={() => {
+                navigate("/game");
+            }}
         >
             New Game
         </Button>
 
         <div className="w-full flex justify-between mt-12">
-            <div>
+            <Button
+                variant="ghost"
+                className="rounded-full text-gray-500"
+            >
                 <Icon type={ICON_TYPE.Settings} />
-            </div>
-            <div>
+            </Button>
+
+            <Button
+                variant="ghost"
+                className="rounded-full text-gray-500"
+            >
                 <Icon type={ICON_TYPE.Cart} />
-            </div>
-            <div>
+            </Button>
+
+            <Button
+                variant="ghost"
+                className="rounded-full text-gray-500"
+            >
                 <Icon type={ICON_TYPE.List} />
-            </div>
-            <div>
+            </Button>
+
+            <Button
+                variant="ghost"
+                className="rounded-full text-gray-500"
+            >
                 <Icon type={ICON_TYPE.Info} />
-            </div>
+            </Button>
         </div>
     </div>;
 };
