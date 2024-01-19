ad735a569d900cc5a53dd4a8d0611c83971743b8
ethan <blazer030@gmail.com>
2024-01-19 20:11:29 +0800

    feature icon list

--- src/presentation/pages/home/Home.tsx
+++ src/presentation/pages/home/Home.tsx
@@ -1,18 +1,35 @@
 import Button from "@/presentation/components/ui/button/button.tsx";
 import { useNavigate } from "react-router-dom";
+import { MdFormatListBulleted, MdInfo, MdSettings, MdShoppingCart } from "react-icons/md";
 
 export const Home = () => {
     const navigate = useNavigate();
 
-    return <div>
-        home
+    return <div className="flex flex-col h-full justify-center items-center gap-4 px-12">
         <Button variant="outline"
-                className="rounded-full"
+                className="rounded-full w-full"
                 onClick={() => {
                     navigate("/game");
                 }}
         >
-            Start
+            New Game
         </Button>
+
+        <div className="w-full flex justify-between mt-12">
+            <div>
+                <Button variant="ghost" className="rounded-full">
+                    <MdSettings size="1.5rem" />
+                </Button>
+            </div>
+            <div>
+                <MdShoppingCart size="1.5rem" />
+            </div>
+            <div>
+                <MdFormatListBulleted size="1.5rem" />
+            </div>
+            <div>
+                <MdInfo size="1.5rem" />
+            </div>
+        </div>
     </div>;
 };
