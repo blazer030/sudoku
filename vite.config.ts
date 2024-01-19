d2ffbb1b1526ac90f45d524184b7414c954587b4
ethan <blazer030@gmail.com>
2024-01-20 03:38:01 +0800

    let vite config can load different env variable by build mode

--- vite.config.ts
+++ vite.config.ts
@@ -1,20 +1,25 @@
-import { defineConfig } from "vite";
+import { defineConfig, loadEnv } from "vite";
 import react from "@vitejs/plugin-react";
 import path from "path";
 import eslintPlugin from "vite-plugin-eslint";
 
+
 // https://vitejs.dev/config/
-export default defineConfig({
-    base: process.env.VITE_BASE_URL,
-    plugins: [
-        react(),
-        eslintPlugin({
-            include: ["./src/**/*.tsx", "./src/**/*.ts"],
-        })
-    ],
-    resolve: {
-        alias: {
-            "@": path.resolve(__dirname, "src"),
+export default ({ mode }) => {
+    process.env = { ...process.env, ...loadEnv(mode, process.cwd()) };
+
+    return defineConfig({
+        base: process.env.VITE_BASE_URL,
+        plugins: [
+            react(),
+            eslintPlugin({
+                include: ["./src/**/*.tsx", "./src/**/*.ts"],
+            })
+        ],
+        resolve: {
+            alias: {
+                "@": path.resolve(__dirname, "src"),
+            }
         }
-    }
-});
+    });
+}
