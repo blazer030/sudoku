0e80acae7518d481f2956967fd2f4ae8e9801c8c
ethan <blazer030@gmail.com>
2024-01-20 00:20:23 +0800

    set cssVariables to false

--- src/presentation/components/ui/button/variants.tsx
+++ src/presentation/components/ui/button/variants.tsx
@@ -1,19 +1,19 @@
 import { cva } from "class-variance-authority";
 
 const buttonVariants = cva(
-    "inline-flex items-center justify-center whitespace-nowrap rounded-md text-sm font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50",
+    "inline-flex items-center justify-center whitespace-nowrap rounded-md text-sm font-medium ring-offset-white transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-sky-950 focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 dark:ring-offset-sky-950 dark:focus-visible:ring-sky-300",
     {
         variants: {
             variant: {
-                default: "bg-primary text-primary-foreground hover:bg-primary/90",
+                default: "bg-sky-900 text-sky-50 hover:bg-sky-900/90 dark:bg-sky-50 dark:text-sky-900 dark:hover:bg-sky-50/90",
                 destructive:
-                    "bg-destructive text-destructive-foreground hover:bg-destructive/90",
+                    "bg-red-500 text-sky-50 hover:bg-red-500/90 dark:bg-red-900 dark:text-sky-50 dark:hover:bg-red-900/90",
                 outline:
-                    "border border-input bg-background hover:bg-accent hover:text-accent-foreground",
+                    "border border-sky-200 bg-white hover:bg-sky-100 hover:text-sky-900 dark:border-sky-800 dark:bg-sky-950 dark:hover:bg-sky-800 dark:hover:text-sky-50",
                 secondary:
-                    "bg-secondary text-secondary-foreground hover:bg-secondary/80",
-                ghost: "hover:bg-accent hover:text-accent-foreground",
-                link: "text-primary underline-offset-4 hover:underline",
+                    "bg-sky-100 text-sky-900 hover:bg-sky-100/80 dark:bg-sky-800 dark:text-sky-50 dark:hover:bg-sky-800/80",
+                ghost: "hover:bg-sky-100 hover:text-sky-900 dark:hover:bg-sky-800 dark:hover:text-sky-50",
+                link: "text-sky-900 underline-offset-4 hover:underline dark:text-sky-50",
             },
             size: {
                 default: "h-10 px-4 py-2",
@@ -26,7 +26,6 @@ const buttonVariants = cva(
             variant: "default",
             size: "default",
         },
-    },
-);
-
+    }
+)
 export default buttonVariants;
