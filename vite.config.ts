81bd713c0e109a3833e8dad99ba9c05d3db9b7e2
ethan <blazer030@gmail.com>
2024-02-16 21:27:00 +0800

    move vitest config to vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,8 @@
-import { defineConfig, loadEnv } from "vite";
+import { loadEnv } from "vite";
 import react from "@vitejs/plugin-react";
 import path from "path";
 import eslintPlugin from "vite-plugin-eslint";
+import { defineConfig } from "vitest/config";
 
 
 // https://vitejs.dev/config/
@@ -20,6 +21,9 @@ export default ({ mode }) => {
             alias: {
                 "@": path.resolve(__dirname, "src"),
             }
-        }
+        },
+        test: {
+            globals: true,
+        },
     });
 }
