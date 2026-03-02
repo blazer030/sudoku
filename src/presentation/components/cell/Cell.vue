e9f789422b6a85d7dd38455ae157cd8945073b27
ethan <blazer030@gmail.com>
2026-03-02 20:24:43 +0800

    ♻️ refactor: 抽取 Cell 元件，封裝格子顯示與選取樣式

---
+++ src/presentation/components/cell/Cell.vue
@@ -0,0 +1,40 @@
+<template>
+  <div
+    :class="[selected ? 'selected bg-sky-100' : '']"
+    class="flex justify-center items-center flex-1 aspect-square"
+  >
+    <div
+      v-if="puzzleCell.isTip"
+      class="text-black text-2xl"
+    >
+      {{ puzzleCell.value }}
+    </div>
+    <div
+      v-else-if="puzzleCell.isEntered"
+      class="text-sky-200 text-2xl"
+    >
+      {{ puzzleCell.input }}
+    </div>
+    <div
+      v-else-if="puzzleCell.hasNotes"
+      class="flex flex-wrap p-1"
+    >
+      <div
+        v-for="(note, noteIndex) in puzzleCell.notes"
+        :key="`note-${noteIndex}`"
+        class="w-1/3 flex justify-center items-center text-xs text-gray-400 aspect-square"
+      >
+        {{ note }}
+      </div>
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+import PuzzleCell from "@/domain/PuzzleCell";
+
+defineProps<{
+    puzzleCell: PuzzleCell;
+    selected?: boolean;
+}>();
+</script>
--- src/presentation/pages/game/Game.vue
+++ src/presentation/pages/game/Game.vue
@@ -15,45 +15,21 @@
         ]"
           class="flex border-sky-200"
       >
-        <div
+        <Cell
             v-for="(puzzleCell, columnIndex) in row"
             :key="`cell-${rowIndex}-${columnIndex}`"
             :data-testid="`cell-${rowIndex}-${columnIndex}`"
+            :puzzle-cell="puzzleCell"
+            :selected="isSelected(rowIndex, columnIndex)"
             :class="[
             columnIndex % 3 !== 0 ? 'border-l-2' : '',
             columnIndex === 8 ? 'border-r-2' : '',
             columnIndex === 0 ? 'border-l-4' : '',
             columnIndex % 3 === 2 ? 'border-r-4' : '',
-            isSelected(rowIndex, columnIndex) ? 'selected' : '',
           ]"
-            class="flex justify-center items-center flex-1 aspect-square border-sky-200"
+            class="border-sky-200"
             @click="selectCell(rowIndex, columnIndex)"
-        >
-          <div
-              v-if="puzzleCell.isTip"
-              class="text-black text-2xl"
-          >
-            {{ puzzleCell.value }}
-          </div>
-          <div
-              v-else-if="puzzleCell.isEntered"
-              class="text-sky-200 text-2xl"
-          >
-            {{ puzzleCell.input }}
-          </div>
-          <div
-              v-else-if="puzzleCell.hasNotes"
-              class="flex flex-wrap p-1"
-          >
-            <div
-                v-for="(note, noteIndex) in puzzleCell.notes"
-                :key="`note-${noteIndex}`"
-                class="w-1/3 flex justify-center items-center text-xs text-gray-400 aspect-square"
-            >
-              {{ note }}
-            </div>
-          </div>
-        </div>
+        />
       </div>
     </div>
     <div class="flex justify-center px-4 gap-2 mb-4">
@@ -90,6 +66,7 @@
 import { reactive, ref } from "vue";
 import Sudoku from "@/domain/Sudoku";
 import Button from "@/presentation/components/ui/button/Button.vue";
+import Cell from "@/presentation/components/cell/Cell.vue";
 
 const sudoku = reactive(new Sudoku());
 const puzzle = sudoku.generate();
