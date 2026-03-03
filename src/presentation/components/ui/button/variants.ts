2a2b23e2f7ac6b15b6a56bc5130ea42c52fe0a07
ethan <blazer030@gmail.com>
2026-03-03 12:10:44 +0800

    🐛 fix: add cursor-pointer to button variants

--- src/presentation/components/ui/button/variants.ts
+++ src/presentation/components/ui/button/variants.ts
@@ -1,7 +1,7 @@
 import { cva } from "class-variance-authority";
 
 const buttonVariants = cva(
-    "inline-flex items-center justify-center whitespace-nowrap rounded-md text-sm font-medium ring-offset-white transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-sky-950 focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 dark:ring-offset-sky-950 dark:focus-visible:ring-sky-300",
+    "inline-flex items-center justify-center whitespace-nowrap rounded-md text-sm font-medium cursor-pointer ring-offset-white transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-sky-950 focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 disabled:cursor-default dark:ring-offset-sky-950 dark:focus-visible:ring-sky-300",
     {
         variants: {
             variant: {
