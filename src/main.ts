a05fe51f001b32e99eb39a2192321e40b783cb02
ethan <blazer030@gmail.com>
2026-04-21 00:25:12 +0800

    🚧 chore: skip pwa registration on native platforms

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,19 @@
 import { createApp } from "vue";
 import { createPinia } from "pinia";
+import { Capacitor } from "@capacitor/core";
 import "@/style/index.css";
 import App from "@/presentation/App.vue";
 import { router } from "@/router";
 
 import { useSettingsStore } from "@/stores/settingsStore";
+
+if (Capacitor.isNativePlatform() && "serviceWorker" in navigator) {
+    void navigator.serviceWorker.getRegistrations().then((registrations) => {
+        registrations.forEach((registration) => {
+            void registration.unregister();
+        });
+    });
+}
 
 const app = createApp(App);
 app.use(createPinia());
